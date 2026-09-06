b4ffb730bed2e1a8784ddeca5b5b0418f2d308c4
Shriram Phatale <shriramphatale833@gmail.com>
2026-09-06 21:05:31 +0530

    chore: add client URL environment config

--- backend/config/env.js
+++ backend/config/env.js
@@ -5,6 +5,7 @@ const ENV = {
     MONGO_URI: process.env.MONGO_URI,
     JWT_SECRET: process.env.JWT_SECRET,
     NODE_ENV: process.env.NODE_ENV,
+    CLIENT_URL: process.env.CLIENT_URL,
     CLOUDINARY_CLOUD_NAME: process.env.CLOUDINARY_CLOUD_NAME,
     CLOUDINARY_API_KEY: process.env.CLOUDINARY_API_KEY,
     CLOUDINARY_API_SECRET: process.env.CLOUDINARY_API_SECRET,
